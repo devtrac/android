999951636620af5cf2d7ff3d9ab87af6bb384955
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-03-08 17:09:49 +0800

    Hello world with Android PhoneGap

---
+++ README.markdown
@@ -0,0 +1,1 @@
+DevTrac Android App
---
+++ src/com/phonegap/Android_devtracActivity.java
@@ -0,0 +1,12 @@
+package com.phonegap;
+
+import android.os.Bundle;
+
+public class Android_devtracActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
