8caf1b49b54c9df7060dd5b82ada3717d52330c3
DevTrac-1 <xfwang@thoughtworks.com>
2012-04-16 16:56:19 +0800

    [rrliu&xfwang] Add photo.reader.js to read photo in base64 encoded

---
+++ assets/www/javascript/interactions/photo.reader.js
@@ -0,0 +1,25 @@
+function PhotoReader(){
+    var _successCallback = function(){};
+    var _errorCallback = function(){};
+
+    this.readFile = function(filePath, successCallback, errorCallback){
+        _successCallback = successCallback;
+        _errorCallback = errorCallback;
+        window.resolveLocalFileSystemURI(filePath, this.onResolveSuccess, this.errorCallback);
+    }
+
+    this.onResolveSuccess = function(fileEntry) {
+        var that = this;
+        fileEntry.file(function(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(evt) {
+                _successCallback(evt.target.result.substring(evt.target.result.indexOf(",") + 1));
+            };
+            reader.readAsDataURL(file);
+        }, function(error){
+            var errorMsg = "Read image data failed. Error code: " + error.code;
+            alert(errorMsg);
+            _errorCallback(errorMsg);
+        });
+    }
+}
